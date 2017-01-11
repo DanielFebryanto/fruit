1efc7fedd8381806c19fa9ec8af7551e14075ff9
daniel <daniel.febryanto@gmail.com>
2017-01-11 23:58:45 +0700

    m

--- application/views/form/formTrx.php
+++ application/views/form/formTrx.php
@@ -56,6 +56,7 @@
 							</div>
 						</div>
 						
+<button type="button" class="btn btn-primary produkBtn" data-toggle="modal" data-target=".bs-example-modal-lg"><i class="fa fa-plus"></i> Produk</button>
 						 <table class="table table-striped">
                                         <thead>
                                             <tr>
@@ -78,12 +79,6 @@
                                                 <td>Thornton</td>
                                                 <td>@fat</td>
                                             </tr>
-                                            <tr>
-                                                <th scope="row">3</th>
-                                                <td>Larry</td>
-                                                <td>the Bird</td>
-                                                <td>@twitter</td>
-                                            </tr>
                                         </tbody>
                                     </table>
 									
@@ -100,8 +95,34 @@
 		</div>
 	</div>
 
+                                <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg">
+                                        <div class="modal-content">
+
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+                                                </button>
+                                                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                                            </div>
+                                            <div class="modal-body">
+                                                <h4>Text in a sssss</h4>
+                                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+                                                <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
 
 </div>
 <script>
-
+$(function(){
+	$(.produkBtn).click(function(){
+		alert("ddd");
+	});
+});
 </script>
